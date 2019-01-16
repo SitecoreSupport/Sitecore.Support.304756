f7768b394525a2687624d1ff7aa1bf45c43698aa
Calvin Williams <40271790+sc-CalvinWilliams@users.noreply.github.com>
2019-01-16 18:18:24 -0500

    Fix issue by encoding icons

---
+++ src/Sitecore.Support.304756/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.304756")]
+[assembly: AssemblyProduct("Sitecore.Support.304756")]
+[assembly: ComVisible(false)]
